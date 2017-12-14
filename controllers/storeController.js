21b3ef764469b94ee7112e1a4d39081d3d9f0058
psasa01 <psasa01@gmail.com>
2017-12-14 16:35:36 +0100

    14122017

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,21 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index', {
     title: 'Index'
   });
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {
+    title: 'Add Store'
+  });
+};
+
+exports.createStore = async(req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+//  res.flash('Success', 'You succesfully saved the store!');
+  res.redirect('/');
 }
