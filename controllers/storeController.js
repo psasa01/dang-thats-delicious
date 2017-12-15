caf485630d9095b326285e6d31f68b5469e40c5b
eminasale <psasa01@gmail.com>
2017-12-15 22:08:27 +0100

    151217

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,20 +2,27 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  res.render('index', {
-    title: 'Index'
-  });
+    res.render('index', {
+        title: 'Index'
+    });
 };
 
 exports.addStore = (req, res) => {
-  res.render('editStore', {
-    title: 'Add Store'
-  });
+    res.render('editStore', {
+        title: 'Add Store'
+    });
 };
 
 exports.createStore = async(req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-//  res.flash('Success', 'You succesfully saved the store!');
-  res.redirect('/');
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `You succesfully saved the ${store.name} store!`);
+    res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async(req, res) => {
+    const stores = await Store.find();
+    res.render('stores', {
+        title: 'Stores',
+        stores
+    });
 }
