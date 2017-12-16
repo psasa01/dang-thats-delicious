44bd20e9e169bcddd2ac50cfeb9c64ec176a4add
psasa01 <psasa01@gmail.com>
2017-12-16 15:01:10 +0100

    161217

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,27 +2,49 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-    res.render('index', {
-        title: 'Index'
-    });
+  res.render('index', {
+    title: 'Index'
+  });
 };
 
 exports.addStore = (req, res) => {
-    res.render('editStore', {
-        title: 'Add Store'
-    });
+  res.render('editStore', {
+    title: 'Add Store'
+  });
 };
 
 exports.createStore = async(req, res) => {
-    const store = await (new Store(req.body)).save();
-    req.flash('success', `You succesfully saved the ${store.name} store!`);
-    res.redirect(`/store/${store.slug}`);
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `You succesfully saved the ${store.name} store!`);
+  res.redirect(`/store/${store.slug}`);
 }
 
 exports.getStores = async(req, res) => {
-    const stores = await Store.find();
-    res.render('stores', {
-        title: 'Stores',
-        stores
-    });
+  const stores = await Store.find();
+  res.render('stores', {
+    title: 'Stores',
+    stores
+  });
+}
+
+exports.editStore = async(req, res) => {
+  const store = await Store.findOne({
+    _id: req.params.id
+  });
+
+  res.render('editStore', {
+    title: 'Edit Store',
+    store
+  })
+}
+
+exports.updateStore = async(req, res) => {
+  const store = await Store.findOneAndUpdate({
+    _id: req.params.id
+  }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated the store. <a href="/store/${store.slug}">View <strong>${store.name}</strong> store</a>`);
+  res.redirect(`/store/${store._id}/edit`);
 }
--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -113,6 +113,43 @@ __webpack_require__(1);
 
 var _bling = __webpack_require__(0);
 
+var _autocomplete = __webpack_require__(9);
+
+var _autocomplete2 = _interopRequireDefault(_autocomplete);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _autocomplete2.default)((0, _bling.$)('#address'), (0, _bling.$)('#lat'), (0, _bling.$)('#lng'));
+
+/***/ }),
+/* 3 */,
+/* 4 */,
+/* 5 */,
+/* 6 */,
+/* 7 */,
+/* 8 */,
+/* 9 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return; //skip if there is no input
+
+  var dropdown = new google.maps.places.Autocomplete(input);
+  dropdown.addListener('place_changed', function () {
+    var place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+}
+
+exports.default = autocomplete;
+
 /***/ })
 /******/ ]);
 //# sourceMappingURL=App.bundle.js.map
