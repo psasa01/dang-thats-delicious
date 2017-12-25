6580dd3f8a96ea0ddbb1381722d8be6d33ed158e
unknown <psasa@gmail.com.com>
2017-12-25 12:46:54 +0100

    251217

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,8 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -14,12 +18,47 @@ exports.logout = (req, res) => {
 
 };
 
-exports.isLoggedIn  = (req,res,next) => {
+exports.isLoggedIn = (req, res, next) => {
   // check if the user is authenticated
-  if(req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     next();
     return;
   }
   req.flash('error', 'You must be logged in!');
   res.redirect('/login');
 };
+
+exports.forgot = async (req, res) => {
+  // 1. see if the user exists
+  const user = await User.findOne({
+    email: req.body.email
+  });
+  if (!user) {
+    req.flash('error', 'No account with that email');
+    return res.redirect('/login');
+  }
+  // 2. set reset tokens and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
+  // 3. send them email with token
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
+  // 4. redirect to login page 
+  res.redirect('/login');
+}
+
+exports.reset = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: {
+      $gt: Date.now()
+    }
+
+
+  })
+  if (!user) {
+    req.flash('error', 'Password reset token is invalid or expired!');
+    return res.redirect('/login');
+  }
+}
